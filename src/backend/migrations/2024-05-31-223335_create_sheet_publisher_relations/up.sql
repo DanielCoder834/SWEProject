813175903431a2a4e634270aa335e51d43fce93f
brooklynschmidt <schmidt.bro@northeastern.edu>
2024-06-12 08:31:26 -0700

    docker

--- src/backend/migrations/2024-05-31-223335_create_sheet_publisher_relations/up.sql
+++ src/backend/migrations/2024-05-31-223335_create_sheet_publisher_relations/up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE publisher_sheets
 (
-    sheets_id   INTEGER REFERENCES sheets (id),
-    publisher_id INTEGER REFERENCES publishers (id),
+    sheets_id   UUID REFERENCES sheets (id),
+    publisher_id UUID REFERENCES publishers (id),
     PRIMARY KEY (sheets_id , publisher_id)
 );
